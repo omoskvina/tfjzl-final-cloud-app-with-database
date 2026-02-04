d2cdd95ed0a6912c805dd9fefcaa24433f07bad6
omoskvina <moskwinaes@gmail.com>
2026-02-04 10:45:34 -0500

    update url, templates, models

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -114,7 +114,7 @@ class Question(models.Model):
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    text = models.CharField(max_length=200)
+    content = models.CharField(max_length=200)
     is_correct = models.BooleanField(default=False)
 
 
